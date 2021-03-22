9f916e5929e37d456430b839171142737f36514f
lazu_xu <xuyexiang@shzhanmeng.com>
2021-03-22 19:35:11 +0800

    save master

---
+++ app/src/test/java/luyao/wanandroid/KotlinTest.kt
@@ -0,0 +1,17 @@
+package luyao.wanandroid
+
+import org.junit.Test
+
+
+class KotlinTest {
+    @Test
+    fun testEqual() {
+//        val a: Short = 999
+//        val b = Integer.valueOf(a.toInt())
+//        val c = Integer.valueOf(a.toInt())
+//        var var4 = Intrinsics.areEqual(b, c)
+//        println(var4)
+
+        var student = Student("李雷",18)
+    }
+}
---
+++ app/src/test/java/luyao/wanandroid/Student.kt
@@ -0,0 +1,6 @@
+package luyao.wanandroid
+
+data class Student(val name: String,
+                   var age: Int
+
+)
